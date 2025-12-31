5ee1760c1e99f44c337225f51ab7b955ecf2c518
Jesse Kela <jessekkela@gmail.com>
2025-12-31 12:07:34 +0200

    Migrate to System.CommandLine

--- src/VideoConverter/ConverterOptions.cs
+++ src/VideoConverter/ConverterOptions.cs
@@ -1,29 +1,10 @@
-using CommandLine;
-using CommandLine.Text;
-
 namespace VideoConverter;
 
 public class ConverterOptions
 {
-    [Option('i', "input", Required = true, HelpText = "The input file to convert.")]
     public string InputFile { get; set; }
 
-    [Option('f', "format", Required = false, HelpText = "Format for the converted output file.")]
     public string OutputFormat { get; set; }
 
-    [Option('o', "output", Required = false, HelpText = "Output path for the converted file.")]
     public string OutputPath { get; set; }
-
-    public static HelpText HandleError<T>(ParserResult<T> result)
-    {
-        var helpText = HelpText.AutoBuild(result, h =>
-        {
-            h.Heading = string.Empty;
-            h.Copyright = string.Empty;
-            h.AutoHelp = false;
-            h.AutoVersion = false;
-            return HelpText.DefaultParsingErrorsHandler(result, h);
-        }, err => err);
-        return helpText;
-    }
 }
